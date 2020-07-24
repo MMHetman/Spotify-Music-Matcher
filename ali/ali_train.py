4e52f410c92ef7269eefbed8ab0b51d6241f190a
MMHetman <michaheta@gmail.com>
2020-07-24 18:35:08 +0200

    Model init

---
+++ ali/ali_train.py
@@ -0,0 +1,70 @@
+from ali.ali_model import ALIModel
+from keras_adversarial import gan_targets
+import numpy as np
+
+
+def get_batch_range(batch_num, batch_size, data_size):
+    return (batch_num * batch_size, min((batch_num + 1) * batch_size, data_size))
+
+
+def rescale_batch(batch):
+    batch += -(np.min(batch))
+    batch /= np.max(batch) / (1 - (-1))
+    batch += (-1)
+    return batch
+
+
+def train_on_data_batch(model, data_file, print_interval):
+    data = np.load(data_file)
+    num_batches = data.shape[0] / batch_size + 1
+    batch_losses = []
+    batch_indices = [
+        get_batch_range(i, batch_size, data.shape[0]) for i in range(num_batches)
+    ]
+    for i in range(len(batch_indices)):
+        start, end = batch_indices[i]
+        # batch = rescale_batch(data[start:end, :, :, None])
+        batch = data[start:end, :, :, None]
+        targets = gan_targets(end - start)
+        targets[0] *= np.random.uniform(0.7, 0.9, end - start)[:, None]
+        targets[3] *= np.random.uniform(0.7, 0.9, end - start)[:, None]
+        losses = model.train_on_batch(batch, targets)
+        batch_losses.append(losses)
+        if i % print_interval == 0:
+            print(losses)
+            print(np.mean(np.reshape(model.predict(data[start:end, :, :, None]), (4, -1)), axis=1))
+
+    return batch_losses
+
+
+def train_one_epoch(model, data_files, print_interval):
+    epoch_losses = []
+    for f in data_files:
+        print('training on {}'.format(f))
+        epoch_losses += train_on_data_batch(model, f, print_interval)
+
+    return epoch_losses
+
+
+def save_weights(model, weight_file):
+    model.save_weights(weight_file)
+
+
+model = ALIModel().model
+batch_size = 101
+epochs = 5
+print_interval = 10
+
+print(model.metrics_names)
+losses = []
+for e in range(epochs):
+    print('epoch {}'.format(e))
+    losses += train_one_epoch(model, ['0_12000.npy', '12000_24000.npy', '24000_36000.npy', '36000_48000.npy',
+                                      '48000_60000.npy', '60000_72000.npy', '72000_84000.npy', '84000_96000.npy',
+                                      '96000_108000.npy'], print_interval)
+    save_weights(model, 'model_weights_8_epoch_{}'.format(e))
+
+print(len(losses))
+with open_file_in_bucket('model_losses_8.csv', 'song-embeddings-dataset') as losses_file:
+    for loss in losses:
+        losses_file.write(','.join([str(l) for l in loss]) + '\n')
