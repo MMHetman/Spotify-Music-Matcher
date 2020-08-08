1c599bc78145c9ece78b32ee91a67bba1948ffff
MMHetman <michaheta@gmail.com>
2020-08-08 18:31:30 +0200

    selecting tracks sample by genre from db

--- backend/data/database_utils.py
+++ backend/data/database_utils.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from sqlalchemy import create_engine
 
 
@@ -15,3 +16,28 @@ def upload_dataframe_as_table(dataframe, table_name):
         print("Table %s created successfully." % table_name)
     finally:
         db_connection.close()
+
+
+def get_tracks_sample_by_genres(sample_size, genres):
+    sql_engine = create_engine('mysql+pymysql://root:password@localhost/spotify_music_matcher', pool_recycle=3600)
+    genres_query = 'SELECT DISTINCT genre FROM artist_n_genre'
+    db_genres = pd.read_sql(genres_query, sql_engine)
+    db_genres = db_genres['genre'].tolist()
+    genres_str = ''
+    for genre in genres:
+        if genre not in db_genres:
+            raise Exception('No such a genre: ' + genre)
+        genres_str += "'" + genre + "', "
+    genres_str = genres_str[:-2]
+    print(genres_str)
+    tracks_query = 'select distinct track_name, artist_name, tracks.track_id, genre ' \
+                   'from tracks join track_n_artist tna on tracks.track_id = tna.track_id ' \
+                   'join artists a on tna.artist_id = a.artist_id join artist_n_genre ang on a.artist_id = ang.artist_id ' \
+                   'where genre in (' + genres_str + ');'
+
+    tracks_df = pd.read_sql(tracks_query, sql_engine)
+    return tracks_df.groupby('genre').apply(lambda s: s.sample(min(len(s), sample_size)))
+
+
+
+
